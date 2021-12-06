27a39a4c9bae82d7b67b094a5f77879878204264
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-06 23:56:41 +0100

    hcaptcha

--- pages/api/contactRequests.js
+++ pages/api/contactRequests.js
@@ -1,17 +1,27 @@
 import base from '@middlewares/common';
 import mailer from '@mailer';
+import axios from 'axios';
+import { verify } from 'hcaptcha';
 
-async function handlePost({ body: { email, message } }, res) {
-  const mailBody = `${email} a laissé un message sur le portfolio : ${message}`;
-  await mailer.sendMail({
-    from: process.env.MAILER_FROM,
-    to: process.env.CONTACT_FORM_RECIPIENT,
-    subject: `Message de la part de ${email} sur ton portefolio`,
-    text: mailBody,
-    html: mailBody,
-    replyTo: email,
-  });
-  res.status(201).send(req.body);
+async function handlePost({ body: { email, message, hcaptchaToken } }, res) {
+  const { success: validCaptcha } = await verify(
+    process.env.HCAPTCHA_SECRET,
+    hcaptchaToken
+  );
+  if (validCaptcha) {
+    const mailBody = `${email} a laissé un message sur le portfolio : ${message}`;
+    await mailer.sendMail({
+      from: process.env.MAILER_FROM,
+      to: process.env.CONTACT_FORM_RECIPIENT,
+      subject: `Message de la part de ${email} sur ton portefolio`,
+      text: mailBody,
+      html: mailBody,
+      replyTo: email,
+    });
+    res.status(201).send('OK');
+  } else {
+    res.status(422).send('invalid hCaptcha token');
+  }
 }
 
 export default base().post(handlePost);
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+
 import axios from 'axios';
 import Layout from '../components/Layout';
 import styles from '../styles/ContactPage.module.css';
@@ -6,14 +8,17 @@ import styles from '../styles/ContactPage.module.css';
 export default function Projects() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [hcaptchaToken, setHcaptchaToken] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('/api/contactRequests', { email, message }).then(() => {
-      alert('Merci, je vous recontacterai au plus vite');
-      setMessage('');
-      setEmail('');
-    });
+    axios
+      .post('/api/contactRequests', { email, message, hcaptchaToken })
+      .then(() => {
+        alert('Merci, je vous recontacterai au plus vite');
+        setMessage('');
+        setEmail('');
+      });
   };
 
   return (
@@ -45,6 +50,14 @@ export default function Projects() {
             />
           </label>
         </div>
+
+        <HCaptcha
+          sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY}
+          onVerify={(token) => {
+            setHcaptchaToken(token);
+          }}
+        />
+
         <button>Envoyer</button>
       </form>
     </Layout>
