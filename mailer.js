f579059a22d8364b2126f6462fcad6dc696114d4
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-06 22:52:22 +0100

    contact form sends mail with mailjet

---
+++ mailer.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+module.exports = transporter;
--- pages/api/contactRequests.js
+++ pages/api/contactRequests.js
@@ -1,7 +1,16 @@
 import base from '@middlewares/common';
+import mailer from '@mailer';
 
-async function handlePost(req, res) {
-  console.log(req.body);
+async function handlePost({ body: { email, message } }, res) {
+  const mailBody = `${email} a laissé un message sur le portfolio : ${message}`;
+  await mailer.sendMail({
+    from: process.env.MAILER_FROM,
+    to: process.env.CONTACT_FORM_RECIPIENT,
+    subject: `Message de la part de ${email} sur ton portefolio`,
+    text: mailBody,
+    html: mailBody,
+    replyTo: email,
+  });
   res.status(201).send(req.body);
 }
 
