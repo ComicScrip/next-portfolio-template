062b95b7e10a4022b0c6a0181da6edc02310a418
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-10 10:10:45 +0100

    tmp

--- middlewares/common.js
+++ middlewares/common.js
@@ -1,10 +1,5 @@
 import nc from 'next-connect';
-import morgan from 'morgan';
-
-morgan.token('reqbody', (req) => JSON.stringify(req.body, null, 2));
-const logger = morgan(
-  ':method :url \nreq.body: :reqbody \n:status - :response-time ms\n'
-);
+import logger from './logger';
 
 export default function base() {
   return nc({
