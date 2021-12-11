418ad54c67241c5ae607cd370d7fc3294b3eb3be
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-11 22:47:10 +0100

    tmp

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,23 +6,13 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { findByEmail, verifyPassword } from '@models/user';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
       async authorize(credentials) {
         const user = await findByEmail(credentials.email);
         if (
@@ -35,7 +25,6 @@ export default NextAuth({
         return null;
       },
     }),
-    // ...add more providers here
   ],
   session: {
     strategy: 'jwt',
