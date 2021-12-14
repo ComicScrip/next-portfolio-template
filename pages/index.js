8e97e5d37ff4252462269262fc48af4ea5b3bdb9
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-14 14:16:59 +0100

    remove home css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import portfolioImg from '../public/images/portfolio.jpg';
 import Layout from '../components/Layout';
 
