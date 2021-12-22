0fe0920cd7c6f7f1d46afe1cef3ada5bdd57f280
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-22 01:21:42 +0100

    ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,15 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     collect: {
-      url: 'http://localhost:3000',
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/login',
+        'http://localhost:3000/signup',
+        'http://localhost:3000/contact',
+        'http://localhost:3000/projects',
+        'http://localhost:3000/projects/1',
+      ],
+      // staticDistDir: '.next/server/pages',
     },
   },
 };
