ed81b189393e56e2e0b04978b75e34ba23f4c9f2
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-12-26 12:04:35 +0100

    ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,10 @@ const baseUrl = process.env.HOST || 'http://localhost:3000';
 const relativeUrlsToCheck = [
   '/',
   '/login',
-  //'/signup',
-  //'contact',
-  //'/projects',
-  //'projects/1',
+  '/signup',
+  'contact',
+  '/projects',
+  'projects/1',
 ];
 
 module.exports = {
@@ -17,12 +17,13 @@ module.exports = {
       url: relativeUrlsToCheck.map((path) => baseUrl + path),
       // staticDistDir: '.next/server/pages',
     },
-    assertions: {
-      'categories:performance': ['warn', { minScore: 1 }],
-      'categories:accessibility': ['error', { minScore: 0.95 }],
-      'categories:best-practices': ['error', { minScore: 0.95 }],
-      'categories:seo': ['error', { minScore: 0.95 }],
-      'categories:pwa': ['error', { minScore: 1 }],
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 1 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
     },
   },
 };
