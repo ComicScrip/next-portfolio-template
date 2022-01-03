6040a70a968272049f40b7bef4b7299270f6dbc5
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-01-03 08:42:46 +0100

    remove lc contents

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['ucarecdn.com'],
   },
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-  },
-});
+};
