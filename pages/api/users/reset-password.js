c421ab7ef1de1c3105ff83191a7530ce18f2d4ee
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-06-14 10:01:51 +0200

    remove lc content

--- pages/api/users/reset-password.js
+++ pages/api/users/reset-password.js
@@ -6,20 +6,6 @@ import {
   verifyPassword,
 } from '../../../models/user';
 
-async function handlePost(req, res) {
-  const { newPassword, newPasswordConfirmation, resetPasswordToken, email } =
-    req.body;
-  if (newPassword !== newPasswordConfirmation)
-    return res.status(400).send('passwords dont match');
-  const user = await findByEmail(email);
-  if (!user) return res.status(404).send();
-  if (!(await verifyPassword(resetPasswordToken, user.resetPasswordToken)))
-    return res.status(400).send('invalid token');
-
-  await updateUser(user.id, {
-    hashedPassword: await hashPassword(newPassword),
-  });
-  res.send('password has been reset');
-}
+async function handlePost(req, res) {}
 
 export default base().post(handlePost);
